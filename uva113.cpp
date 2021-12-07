952566503b6453ac857a0316ba674359a5fc218a
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-07 12:24:11 +0800

    Add files via upload

---
+++ uva113.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+   double n, p;
+   while (cin >> n >> p)
+   {
+      cout << pow(p, 1 / n) << endl;
+   }
+   return 0;
+}
