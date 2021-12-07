1e28dbef9aadf7cd0e7e832623e2ebffa16be362
YuTing-Fang1999 <66452317+YuTing-Fang1999@users.noreply.github.com>
2021-12-07 13:19:11 +0800

    Update uva113.cpp

--- uva113.cpp
+++ uva113.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
   double n, p;
   while( cin>>n>>p )
-  	//converting to %.0lf, it's rounded to the nearest integer.
+    //converting to %.0lf, it's rounded to the nearest integer.
     printf( "%.0lf\n", pow( p, 1.0/n ) );
   return 0;
 }
